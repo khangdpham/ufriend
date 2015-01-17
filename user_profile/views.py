e3f7a7f0929b36e41060ec87dc5959218d9e9e64
Khang Pham <khangpham@Khangs-MacBook-Pro.local>
2015-01-17 15:14:53 -0800

    Updated

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render,render_to_response
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login as auth_login , views
 from django.http import HttpResponseRedirect
+from events.models import Event
  
 def view_profile(request):
 	username = request.user
 	if request.user.is_authenticated():
-		context = { 'username' : username , }
+		events = Event.objects.all()
+        	context = { 'username' : username ,'events':events }
 		return render_to_response(
 			'profile/profile.html',
 			context,
